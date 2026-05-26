e1989a2580a60d96ee1b4715000eb8b6ce0409f9
optimum42 <holgermassek@gmail.com>
2026-05-26 15:28:35 +0200

    Website Output of all Blog Posts

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
-import json_storage
+from flask import Flask, render_template
+from blog_manager import BlogPostManagerJSON
 
 app = Flask(__name__)
+blog_manager = BlogPostManagerJSON()
 
 
-@app.route('/')
+@app.route('/helloworld')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/")
+def index():
+    blog_posts = blog_manager.read_all_posts()
+    return render_template("index.html", blog_posts=blog_posts)
 
 
 if __name__ == '__main__':

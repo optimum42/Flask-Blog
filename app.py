1081e278d7b5367548a81409b0d3df51dde793fe
optimum42 <holgermassek@gmail.com>
2026-05-26 15:49:33 +0200

    Add Feature added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from blog_manager import BlogPostManagerJSON
 
 app = Flask(__name__)
@@ -13,6 +13,21 @@ def hello_world():
 def index():
     blog_posts = blog_manager.read_all_posts()
     return render_template("index.html", blog_posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form["author"]
+        title = request.form["title"]
+        content = request.form["content"]
+        blog_manager.add_post(
+            author=author,
+            title=title,
+            content=content
+        )
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
